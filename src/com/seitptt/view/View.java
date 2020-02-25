494755221d4d1fd36858493ba516d54ba14b35b0
agill123 <59920224+agill123@users.noreply.github.com>
2020-02-25 16:05:55 +0000

    Created View

---
+++ src/com/seitptt/view/View.java
@@ -0,0 +1,61 @@
+package com.seitptt.view;
+
+import javax.swing.JFrame;
+/**
+ * View - creates the GUI view for the teacher admin application
+ */
+public class View extends JFrame {
+	
+	
+	/**
+	 * view constructor sets the model and controller
+	 * and creates homescreen view
+	 */
+	public View() {
+		
+	}
+	
+	/**
+	 * creates class director screen
+	 */
+	public void createClassDirScreen() {
+		
+	}
+	
+	/**
+	 * updates class director screen
+	 */
+	public void updateClassDirScreen() {
+		
+	}
+	/**
+	 * creates admin screen
+	 */
+	public void createAdminScreen() {
+		
+	}
+	/**
+	 * updates admin screen
+	 */
+	public void updateAdminScreen() {
+		
+	}
+	/**
+	 * creates PTT director screen
+	 */
+	public void createPTTDirScreen() {
+		
+	}
+	/**
+	 * updates PTT director screen
+	 */
+	public void updatePTTDirScreen() {
+		
+	}
+	//test main will remove
+	public static void main(String[] args) {
+		
+	}
+	
+
+}
