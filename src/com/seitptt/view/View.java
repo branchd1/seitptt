5aa7374106c72a36f68713dffe367b0cc6d3236b
agill123 <59920224+agill123@users.noreply.github.com>
2020-02-25 17:02:53 +0000

    constructor changes, some class attributes and getters

--- src/com/seitptt/view/View.java
+++ src/com/seitptt/view/View.java
@@ -24,6 +24,9 @@ public class View extends JFrame {
 
 	// view unit
 	final int UNIT = 30;
+    //homescreen class attributes
+	 private JTextField usernameEntry;
+	 private JTextField passwordEntry;
 
 	/**
 	 * view constructor sets the model and controller and creates homescreen view
@@ -35,56 +38,74 @@ public class View extends JFrame {
 		this.setSize(35 * UNIT, 20 * UNIT);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(new GridLayout(1, 1));
+		createHomeScreen();
 		
-		//create and add homepage panel
-		homePanel=new JPanel();
-		homePanel.setLayout(new GridLayout(3,1));
-		this.add(homePanel);
 		
-		//create and add app logo panel
-		JLabel appLogo=new JLabel("TeacherAdmin App",SwingConstants.CENTER);
-        appLogo.setFont(new java.awt.Font("Arial", Font.BOLD, 75));
-        homePanel.add(appLogo);
-        
-        //create and add login panel
-        JPanel loginPanel= new JPanel();
-        loginPanel.setLayout(new GridLayout(4,1));
-        homePanel.add(loginPanel);
-        
-        //create and add username and password panels to login panel
-        JPanel usernamePanel=new JPanel();
-        JLabel usernameLabel=new JLabel("Username:");
-        JTextField usernameEntry= new JTextField();
-        usernameEntry.setColumns(20);
-        usernamePanel.add(usernameLabel);
-        usernamePanel.add(usernameEntry);
-        loginPanel.add(usernamePanel);
-        
-        JPanel passwordPanel=new JPanel();
-        JLabel passwordLabel=new JLabel("Password:");
-        JTextField passwordEntry= new JTextField();
-        passwordEntry.setColumns(20);
-        passwordPanel.add(passwordLabel);
-        passwordPanel.add(passwordEntry);
-        loginPanel.add(passwordPanel);
-     
-        //create and add login button panel to login panel
-        JPanel loginButtonPanel= new JPanel();
-        loginButtonPanel.setLayout(new GridBagLayout());
-        JButton loginButton = new JButton("Login");
-        //COMMENTED OUT CONTROLLER ACTION LISTENER
-        //loginButton.addActionListener(l);
-        loginButtonPanel.add(loginButton);
-        loginPanel.add(loginButtonPanel);
         
 
+	}
+	/**
+	 * creates home screen
+	 */
+	private void createHomeScreen() {
+		//create and add homepage panel
+				homePanel=new JPanel();
+				homePanel.setLayout(new GridLayout(3,1));
+				this.add(homePanel);
+				
+				//create and add app logo panel
+				JLabel appLogo=new JLabel("TeacherAdmin App",SwingConstants.CENTER);
+		        appLogo.setFont(new java.awt.Font("Arial", Font.BOLD, 75));
+		        homePanel.add(appLogo);
+		        
+		        //create and add login panel
+		        JPanel loginPanel= new JPanel();
+		        loginPanel.setLayout(new GridLayout(4,1));
+		        homePanel.add(loginPanel);
+		        
+		        //create and add username and password panels to login panel
+		        JPanel usernamePanel=new JPanel();
+		        JLabel usernameLabel=new JLabel("Username:");
+		        usernameEntry= new JTextField();
+		        usernameEntry.setColumns(20);
+		        usernamePanel.add(usernameLabel);
+		        usernamePanel.add(usernameEntry);
+		        loginPanel.add(usernamePanel);
+		        
+		        JPanel passwordPanel=new JPanel();
+		        JLabel passwordLabel=new JLabel("Password:");
+		        passwordEntry= new JTextField();
+		        passwordEntry.setColumns(20);
+		        passwordPanel.add(passwordLabel);
+		        passwordPanel.add(passwordEntry);
+		        loginPanel.add(passwordPanel);
+		     
+		        //create and add login button panel to login panel
+		        JPanel loginButtonPanel= new JPanel();
+		        loginButtonPanel.setLayout(new GridBagLayout());
+		        JButton loginButton = new JButton("Login");
+		        //COMMENTED OUT CONTROLLER ACTION LISTENER
+		        //loginButton.addActionListener(l);
+		        loginButtonPanel.add(loginButton);
+		        loginPanel.add(loginButtonPanel);
+	}
+
+	/**
+	 * returns username from home screen
+	 */
+	public String getUsername() {
+		return usernameEntry.getText();
+	}
+	
+	public String getPassword() {
+		return passwordEntry.getText();
 	}
 
 	/**
 	 * creates class director screen
 	 */
 	public void createClassDirScreen() {
-
+      
 	}
 
 	/**
