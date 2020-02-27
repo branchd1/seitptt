71fe0a3258c8498d782ffb64a2ea0d9a91c7ee84
Chika Hope <elumeziemchikahope@gmail.com>
2020-02-27 17:29:46 +0000

    Core.java updated

--- src/com/seitptt/model/Core.java
+++ src/com/seitptt/model/Core.java
@@ -4,14 +4,20 @@ import java.util.ArrayList;
 
 import com.seitptt.model.authentication.Auth;
 import com.seitptt.model.database.Database;
+import com.seitptt.model.personnel.ClassDirector;
 import com.seitptt.model.personnel.Employee;
 import com.seitptt.model.personnel.ListOfEmployees;
+import com.seitptt.model.processes.ListOfSemesters;
+import com.seitptt.model.processes.ListOfTeachingRequirements;
 import com.seitptt.model.processes.Semester;
+import com.seitptt.model.processes.TeachingRequirement;
+import com.seitptt.visitors.PrintToDatabaseVisitor;
 
 public class Core {
 	
 	private Semester currentSemester;
 	private Employee currentUser;
+	
 	
 	public Core() {
 		Database.LoadCaches();
@@ -56,21 +62,41 @@ public class Core {
 		
 		
 		
-		System.out.println("-------------------\nASSERT\n-------------------");
-		ArrayList<Semester> listOfSemesters = Database.getSemestersFromDB();
-		for (Semester s : listOfSemesters) {
-			System.out.println(s.getNumber() + " " + s.getYear());
-		}
-		System.out.println("-------------------\nEQUALS\n-------------------");
-		System.out.println("1 2020\n" + 
-				"2 2020\n" + 
-				"1 2021\n" + 
-				"2 2021\n" + 
-				"1 2022");
-		System.out.println("-------------------\nEND\n-------------------");
+//		System.out.println("-------------------\nASSERT\n-------------------");
+//		ArrayList<Semester> listOfSemesters = Database.getSemestersFromDB();
+//		for (Semester s : listOfSemesters) {
+//			System.out.println(s.getNumber() + " " + s.getYear());
+//		}
+//		System.out.println("-------------------\nEQUALS\n-------------------");
+//		System.out.println("1 2020\n" + 
+//				"2 2020\n" + 
+//				"1 2021\n" + 
+//				"2 2021\n" + 
+//				"1 2022");
+//		System.out.println("-------------------\nEND\n-------------------");
 	}
 	
-	public listOfSemesters
+	public void addTeachingRequirement(TeachingRequirement tr) {
+		if (!(currentUser instanceof ClassDirector)) {
+			throw new RuntimeException("Sorry current user is not a ClassDirector");
+		}else {					
+			PrintToDatabaseVisitor visitor = new PrintToDatabaseVisitor();
+			tr.accept(visitor);			
+		}
+	}
+	
+	
+	public ListOfTeachingRequirements getListOfTeachingRequirements() {
+		if (!(currentUser instanceof ClassDirector)) {
+			throw new RuntimeException("Sorry current user is not a ClassDirector");
+		}else {
+			return Database.getTeachingRequirementsFromDB();
+		}
+	}
+	
+	public ListOfSemesters getListOfSemesters() {
+		return Database.getSemestersFromDB();
+	}
 	
 	public Employee login(String username, String password) {
 		return Auth.login(username, password);
