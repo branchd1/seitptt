90d2382d5e71503af27b3114b6531b350c502d15
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-03-02 11:45:33 +0000

    comments

--- src/com/seitptt/model/personnel/ClassDirector.java
+++ src/com/seitptt/model/personnel/ClassDirector.java
@@ -1,6 +1,16 @@
 package com.seitptt.model.personnel;
 
+/**
+ * represents a class director
+ * @author arnoldumakhihe 2445734u
+ *
+ */
 public class ClassDirector extends Employee{
+	/**
+	 * class director constructor
+	 * @param firstName class director first name
+	 * @param lastName class director last name
+	 */
 	public ClassDirector(String firstName, String lastName) {
 		super(firstName, lastName);
 	}
--- src/com/seitptt/model/personnel/Teacher.java
+++ src/com/seitptt/model/personnel/Teacher.java
@@ -1,16 +1,36 @@
 package com.seitptt.model.personnel;
 
+/**
+ * represents a teacher
+ * @author arnoldumakhihe 2445734U
+ *
+ */
 public class Teacher extends Employee{
+	/**
+	 * represents if the teacher is trained
+	 */
 	private boolean trained = false;
 	
+	/**
+	 * teacher constructor 
+	 * @param firstName teacher first name
+	 * @param lastName teacher last name
+	 */
 	public Teacher(String firstName, String lastName) {
 		super(firstName, lastName);
 	}
 	
+	/**
+	 * train a teacher
+	 */
 	public void train() {
 		this.trained = true;
 	}
 	
+	/**
+	 * check if a teacher is trained
+	 * @return true if teacher is trained, and false otherwise
+	 */
 	public boolean isTrained() {
 		return this.trained;
 	}
