33b31418d872cf12c96d25d661ddf0357fcc569f
Estelle <bokyunglee33@gmail.com>
2020-03-03 13:38:06 +0000

    latest version of controller

--- src/com/seitptt/controller/Controller.java
+++ src/com/seitptt/controller/Controller.java
@@ -18,12 +18,12 @@ import com.seitptt.model.processes.TeachingRequirement;
 import com.seitptt.view.View;
 
 public class Controller implements ActionListener, ListSelectionListener{
-	
+
 	private Core model;
 	private View view;
 	private Semester chosenSemester;
 	private int removeReqID, selectedFilterIndexForAdmin;
-	
+
 	/**
 	 * @param Core model
 	 * @param View view
@@ -31,20 +31,32 @@ public class Controller implements ActionListener, ListSelectionListener{
 	public Controller(Core model) {
 		this.model=model;
 	}
-	
+
 	public void setView(View view) {
 		this.view=view;
 	}
-	
+
+	/**
+	 * For Class Director screen when getSemesterSelector passes actionListener
+	 * @return Semester chosenSemester
+	 * */
 	public Semester getChosenSemester() {
 		return chosenSemester;
 	}
 
+	/**
+	 * For Administrator screen when getTrainingSelector passes actionListener
+	 * @return int selextedFilterIndexForAdmin
+	 * */
 	public int getSelectedFilterIndexForAdmin() {
 		return selectedFilterIndexForAdmin;
 	}
 
+	/**
+	 * actionPerformed called from view (actionListener)
+	 * */
 	public void actionPerformed(ActionEvent e) {
+		//while(true) {
 		//1. login to each type of user
 		if(e.getSource()==view.classDirLogin()) {
 			view.createClassDirScreen();
@@ -53,7 +65,7 @@ public class Controller implements ActionListener, ListSelectionListener{
 		}else if(e.getSource()==view.pttDirLogin()) {
 			view.createPTTDirScreen();
 		}
-		
+
 		//2. ClassDirectorView
 		//2.1. selecting semester
 		if(e.getSource()==view.getSemesterSelector()) {
@@ -65,10 +77,10 @@ public class Controller implements ActionListener, ListSelectionListener{
 					chosenSemester=currSemester;
 				}
 			}
-			
+
 			view.enableClassList();
 		}
-		
+
 		//2.2. add requirements
 		if(e.getSource()==view.getAddRequirementButton()) {
 			Classes chosenClass = null;
@@ -76,7 +88,7 @@ public class Controller implements ActionListener, ListSelectionListener{
 			String currClassString=(String)view.getClassSelector().getSelectedItem();
 			String[] splitCurrClass=currClassString.split(" ");
 			String classCode=splitCurrClass[0];
-			
+
 			for(Classes currClass:listOfClasses) {
 				if(currClass.getCode().equals(classCode)) {//listOfClasses.find(currClassIndex)
 					chosenClass=currClass;					
@@ -87,7 +99,7 @@ public class Controller implements ActionListener, ListSelectionListener{
 				}
 			}
 		}
-		
+
 		//2.4. remove requirements
 		if(e.getSource()==view.getRemoveRequirementButton()) {
 			ListOfTeachingRequirements listOfRequirements=model.getListOfTeachingRequirements();
@@ -100,16 +112,16 @@ public class Controller implements ActionListener, ListSelectionListener{
 			}
 			view.updateClassDirScreen();
 		}	
-		
+
 		//3. Administrator Screen
 		TeachingRequirement addTeachersInReq;
-		
+
 		//3.1. (JComboBox) filter list of teachers
 		if(e.getSource()==view.getTrainingSelector()) {
 			selectedFilterIndexForAdmin=view.getTrainingSelectedIndex();
 			view.trainingUpdate();
 		}
-		
+
 		//3.2. (JComboBox) choose class requirements
 		if(e.getSource()==view.getRequirementSelector()) {
 			int chosenReqIndex=view.getRequirementSelectedIndex();
@@ -118,30 +130,39 @@ public class Controller implements ActionListener, ListSelectionListener{
 			for(TeachingRequirement i:teachingReqirementList) {
 				if(j==chosenReqIndex) {
 					addTeachersInReq=i;
-//					i.getNumOfTeachers()--;
+					//i.getNumOfTeachers()--;
 				}
 				j++;
 			}
 		}
-		//3.1. add teachers
-		//3.2. train teachers
+
+		//3.3. add teachers
+		if(e.getSource()==view.adminAddTeachersButton()) {
+			//add selected teachers can
+			//sub number of teachers from current teaching requirement
+			
+		}
+		//3.4. train teachers
+
+
 		//4. PTT Director Screen
 		//4.1. filter requirements (All, Pending)
 		if(e.getSource()==view.pttDirRequirementsFilter()) {
 			String filter=view.pttDirRequirementsFilter().toString();
 			ListOfTeachingRequirements listOfRequirements=model.getListOfTeachingRequirements();
-			
+
 		}
+		//}
 	}
 
 	/**
-	 * For addListSelectionListener.
-	 * selects from list and find requested list's id in removeReqID
+	 * value changed when view calls addListSelectionListener.
 	 * @param ListSelectionEvent e
 	 * */
 	@Override
 	public void valueChanged(ListSelectionEvent e) {
 		//2.3. find id of remove requirements 
+		//selects from list and find requested list's id in removeReqID
 		if(e.getSource()==view.getRequirementsList()) {
 			int removeReqIndex;
 			int reqIndex=view.getRequirementsList().getSelectedIndex();
